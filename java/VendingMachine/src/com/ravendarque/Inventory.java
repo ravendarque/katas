adc91ecdad913bc3e26df94e59fd83a2698fc899
Nix Crabtree <nix@ravendarque.com>
2019-10-09 17:38:14 +0100

    Added test and code for Inventory with one item

--- java/VendingMachine/src/com/ravendarque/Inventory.java
+++ java/VendingMachine/src/com/ravendarque/Inventory.java
@@ -5,8 +5,15 @@ import java.util.List;
 
 class Inventory {
 
+    private final ArrayList<Item> items = new ArrayList<>();
+
+    void addItem(Item testItem) {
+
+        items.add(testItem);
+    }
+
     List<Item> getItems() {
 
-        return new ArrayList<>();
+        return items;
     }
 }
--- java/VendingMachine/test/com/ravendarque/InventoryShould.java
+++ java/VendingMachine/test/com/ravendarque/InventoryShould.java
@@ -19,4 +19,21 @@ class InventoryShould {
 
         assertEquals(expectedInventoryList, actualInventoryList);
     }
+
+    @Test
+    void ProvideOneItemInInventoryListWhenOneItemIsInInventory() {
+
+        String dummyName = "Test item";
+        double dummyPrice = 0;
+        Item testItem = new Item(dummyName, dummyPrice);
+
+        List<Item> expectedInventoryList = new ArrayList<>();
+        expectedInventoryList.add(testItem);
+
+        Inventory inventory = new Inventory();
+        inventory.addItem(testItem);
+        List<Item> actualInventoryList = inventory.getItems();
+
+        assertEquals(expectedInventoryList, actualInventoryList);
+    }
 }
